d76b40677524cf920a6ed837563a9622f6237344
mohannad <mohannadward824@gmail.com>
2024-02-17 21:10:11 +0200

    edit middleware file

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,8 @@ export default authMiddleware({
   publicRoutes: [
     "/",
     "/api/webhooks/clerk",
-    "/api/webhooks/stripe"
+    "/api/webhooks/stripe",
+    "/api/webhooks(.*)"
   ],
 });
 
